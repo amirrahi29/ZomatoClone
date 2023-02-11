044035f2fbdbc2602cdd90718f49a6224372ccfb
Amir Rahi <amirrahi29@gmail.com>
2023-02-11 19:27:51 +0530

    added

---
+++ src/GlobalEquipments/CommonInputField.js
@@ -0,0 +1,34 @@
+import { View, Image } from 'react-native'
+import React from 'react'
+import { TextInput } from 'react-native-gesture-handler'
+import { Orientation } from './AllDimensions'
+import AllIcons from './AllIcons'
+
+const CommonInputField = ({placeholder,onchangeMobileText}) => {
+    return (
+        <View style={{
+            height: 60,
+            borderColor: 'black',
+            borderRadius: 8,
+            padding: 8,
+            borderWidth: 1,
+            flexDirection: Orientation.horizontal
+        }}>
+
+            <Image source={AllIcons.logo} style={{ flex: 1, height: 50, alignSelf: 'center' }} />
+
+            <TextInput
+                style={{
+                    flex: 5,
+                    color: 'black'
+                }}
+                placeholder={placeholder}
+                placeholderTextColor={'black'}
+
+            />
+
+        </View>
+    )
+}
+
+export default CommonInputField
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,43 @@
-import { View, Image, StyleSheet } from 'react-native'
-import React from 'react';
+import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react';
 import CommonCss from '../GlobalEquipments/css/CommonCss';
 import AllIcons from '../GlobalEquipments/AllIcons';
-import { AllDimensions } from '../GlobalEquipments/AllDimensions';
+import CommonInputField from '../GlobalEquipments/CommonInputField';
+import Checkbox from 'expo-checkbox';
+import { ScrollView } from 'moti';
+
 
 const Login = () => {
+
+  const [gender, setGender] = useState(false);
+
   return (
     <View style={CommonCss.container}>
-        <Image source={AllIcons.food1} style={styles.imageStyle} />
+      <Image source={AllIcons.food1} style={styles.imageStyle} />
+
+      <ScrollView style={{
+        margin: 8
+      }}>
+
+        <CommonInputField placeholder={'Enter mobile number'} onchangeMobileText={{}} />
+        
+        <View style={{ marginTop: 16 }}></View>
+
+        <TouchableOpacity onPress={()=>setGender(!gender)} style={{flexDirection:'row'}}>
+
+          <Checkbox
+            style={{color:'black'}}
+            value={gender}
+            onValueChange={setGender}
+            color={gender ? '#4630EB' : undefined}
+          />
+
+          <Text style={{color:'black',marginLeft:16}}>Male {gender}</Text>
+
+        </TouchableOpacity>
+
+      </ScrollView>
+
     </View>
   )
 }
@@ -15,10 +45,10 @@ const Login = () => {
 export default Login;
 
 const styles = StyleSheet.create({
-  imageStyle:{
-    height:300,
-    width:'100%',
-    borderBottomLeftRadius:Platform.OS != 'IOS'?16:0,
-    borderBottomRightRadius:Platform.OS != 'IOS'?16:0,
+  imageStyle: {
+    height: 300,
+    width: '100%',
+    borderBottomLeftRadius: Platform.OS != 'IOS' ? 16 : 0,
+    borderBottomRightRadius: Platform.OS != 'IOS' ? 16 : 0,
   }
 });
