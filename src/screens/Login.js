721326a9aaf780fae218b5d73584d382ad4d8124
Amir Rahi <amirrahi29@gmail.com>
2023-02-11 19:59:40 +0530

    added

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,15 +1,20 @@
-import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { View, Image, StyleSheet, Text, TouchableOpacity, ScrollView } from 'react-native'
 import React, { useState } from 'react';
 import CommonCss from '../GlobalEquipments/css/CommonCss';
 import AllIcons from '../GlobalEquipments/AllIcons';
 import CommonInputField from '../GlobalEquipments/CommonInputField';
 import Checkbox from 'expo-checkbox';
-import { ScrollView } from 'moti';
+import RadioButtonGroup, { RadioButtonItem } from "expo-radio-button";
+import { TextInput } from 'react-native-gesture-handler';
 
 
 const Login = () => {
 
+  //checkbox
   const [gender, setGender] = useState(false);
+
+  //radio
+  const [current, setCurrent] = useState("Male");
 
   return (
     <View style={CommonCss.container}>
@@ -20,21 +25,71 @@ const Login = () => {
       }}>
 
         <CommonInputField placeholder={'Enter mobile number'} onchangeMobileText={{}} />
-        
+
         <View style={{ marginTop: 16 }}></View>
 
-        <TouchableOpacity onPress={()=>setGender(!gender)} style={{flexDirection:'row'}}>
 
+
+
+
+        <TouchableOpacity onPress={() => setGender(!gender)} style={{ flexDirection: 'row' }}>
           <Checkbox
-            style={{color:'black'}}
+            style={{ color: 'black' }}
             value={gender}
             onValueChange={setGender}
             color={gender ? '#4630EB' : undefined}
           />
 
-          <Text style={{color:'black',marginLeft:16}}>Male {gender}</Text>
-
+          <Text style={{ color: 'black', marginLeft: 16 }}>Male {gender}</Text>
         </TouchableOpacity>
+
+
+
+
+
+
+        <View style={{ marginTop: 50 }}>
+          <RadioButtonGroup
+            containerStyle={{ marginBottom: 10 }}
+            selected={current}
+            onSelected={(value) => setCurrent(value)}
+            radioBackground="green"
+          >
+            <RadioButtonItem
+              value="Male"
+              label={
+                <Text style={{ color: "red" }}>Male</Text>
+              } />
+            <RadioButtonItem
+              value="Female"
+              label={
+                <Text style={{ color: "red" }}>Female</Text>
+              }
+            />
+          </RadioButtonGroup>
+        </View>
+        <Text style={{ color: 'red' }}>{current}</Text>
+
+
+        <Text style={{ marginTop: 50 }}>{current}</Text>
+
+
+              
+        <TextInput
+          placeholder='Enter comments'
+          placeholderTextColor={'black'}
+          multiline={true}
+          numberOfLines={5}
+          style={{
+            borderWidth:1,
+            borderColor:'green',
+            borderRadius:16,
+            verticalAlign:'top',
+            color:'black',
+            padding:8
+          }}
+        />
+
 
       </ScrollView>
 
@@ -46,7 +101,7 @@ export default Login;
 
 const styles = StyleSheet.create({
   imageStyle: {
-    height: 300,
+    height: 50,
     width: '100%',
     borderBottomLeftRadius: Platform.OS != 'IOS' ? 16 : 0,
     borderBottomRightRadius: Platform.OS != 'IOS' ? 16 : 0,
